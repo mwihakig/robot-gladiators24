07d58cd569d42a376d2d19942c55804064788510
mwihakig <97566064+mwihakig@users.noreply.github.com>
2024-01-22 03:19:45 -0500

    Add files via upload

---
+++ assets/js/game.js
@@ -0,0 +1,16 @@
+var playerName = window.prompt ("What is your name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roberto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function(){
+    window.alert("Welcome to Robot Gladiators!");
+};
+
+fight();
